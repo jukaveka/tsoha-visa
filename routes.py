7d8b1086d15bd559fe375f423ad94e006e7116d0
Juho Kavekari <juhokavekari@hotmail.com>
2024-08-08 19:39:03 +0300

    Added routing for registration, login and logout

--- routes.py
+++ routes.py
@@ -1,2 +1,52 @@
 from app import app
+from flask import render_template, redirect, request
+import users #, quizzes
 
+@app.route("/")
+def index():
+	return(render_template("index.html"))
+
+"""
+@app.route("/quizzes/")
+def quizzes():
+
+
+@app.route("/quizzes/new/")
+def new_quiz():
+
+
+@app.route("/quizzes/<int:id>")
+def quiz():
+"""
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+	if request.method == "GET":
+		return render_template("register.html")
+	if request.method == "POST":
+		username = request.form["username"]
+		password1 = request.form["password1"]
+		password2 = request.form["password2"]
+		if password1 != password2:
+			return render_template("error.html", message="Salasanat eroavat")
+		if users.register(username, password1):
+			return redirect("/")
+		else:
+			return render_template("error.html", message="Tunnusten luonti epäonnistui")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+	if request.method == "GET":
+		return render_template("login.html")
+	if request.method == "POST":
+		username = request.form["username"]
+		password = request.form["password"]
+		if users.login(username, password):
+			return redirect("/")
+		else:
+			return render_template("error.html", message="Kirjautuminen epäonnistui. Tarkista antamasi käyttäjätunnus tai salasana, ja yritä uudestaan")
+
+@app.route("/logout")
+def logout():
+	users.logout()
+	return redirect("/")
