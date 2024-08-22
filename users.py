2e9295e2649cb209461516a2594757fdcb1a1c66
Juho Kavekari <juhokavekari@hotmail.com>
2024-08-22 20:24:53 +0300

    Spacing some functions

--- users.py
+++ users.py
@@ -5,32 +5,43 @@ from sqlalchemy.sql import text
 
 
 def login(username, password):
+	
 	sql = text("SELECT id, nickname, password FROM users WHERE nickname=:username")
 	result = db.session.execute(sql, {"username":username})
 	user = result.fetchone()
+	
 	if not user:
+		
 		return False
 	else:
+		
 		if check_password_hash(user.password, password):
+			
 			session["user_id"] = user.id
 			return True
+		
 		else:
+			
 			return False
 
 def logout():
 	del session["user_id"]
 
 def register(username, password):
+	
 	hash_value = generate_password_hash(password)
+	
 	try:
 		sql = text("INSERT INTO users (nickname,password) VALUES (:username,:password)")
 		db.session.execute(sql, {"username":username, "password":hash_value})
 		db.session.commit()
 	except:
 		return False
+	
 	return login(username, password)
 
 def user_id():
+	
 	return session.get("user_id",0)
 
 def get_user_information(user_id):
